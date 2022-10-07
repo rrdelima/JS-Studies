99aed445f64b639ee66f2851eec217a40ed2177e
rrdelima <rrdelima@gmail.com>
2022-10-07 20:32:33 -0300

    getElementById

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
+// var - problema de escopo - obsoleto
+// let - valor dinâmico
+// const - constante - valor não muda
+
+let soma = 0;
+let somaElem = document.getElementById('soma');
+
+somaElem.innerHTML = soma;
+
 function somar(){
-    console.log('somando...');
+    soma = soma + 1;
+    somaElem.innerHTML = soma;
 }
 
 
