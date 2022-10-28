65bd209031ade7456750ab6bbd6efc3fbdab5a53
rrdelima <rrdelima@gmail.com>
2022-10-27 22:01:30 -0300

    Escopo let e const

--- scripts.js
+++ scripts.js
@@ -1,10 +1,19 @@
-var name = "Rodolfo" //declaracao Global
+//var name = "Rodolfo" //declaracao Global
 
 function foo() {
-    //var name = "Rodolfo"; //declaracao Local
+    var name = "Rodolfo"; //declaracao Local
     console.log(name);
 }
 
+if(true){
+    //var teste = "Rodrigues"; // visivel globalmente. Vaza do bloco para fora...
+    //const teste = "Rodrigues"; // visivel somente no bloco
+    let teste = "Rodrigues"; // visivel somente no bloco
+    teste = "Lima";
+
+    console.log('---', teste);
+}
+
 foo();
-console.log('---', name);
+console.log('---', teste);
 
